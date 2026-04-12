e922096af44d17445c0e2d88ca989a98c3e06e2f
s1388379 <s1388379@live.hkmu.edu.hk>
2026-04-13 02:33:07 +0800

    heap.py

--- Task_2/heap.py
+++ Task_2/heap.py
@@ -12,34 +12,56 @@ class MaxHeap:
     def _right_child_index(self, i):
         return 2 * i + 2
 
+    def _heapify_up(self, index):
+        while index > 0:
+            parent = self._parent_index(index)
+            if self.data[index] > self.data[parent]:
+                self.data[index], self.data[parent] = self.data[parent], self.data[index]
+                index = parent
+            else:
+                break
+
+    def _heapify_down(self, index):
+        size = len(self.data)
+        while True:
+            left = self._left_child_index(index)
+            right = self._right_child_index(index)
+            largest = index
+
+            if left < size and self.data[left] > self.data[largest]:
+                largest = left
+
+            if right < size and self.data[right] > self.data[largest]:
+                largest = right
+
+            if largest != index:
+                self.data[index], self.data[largest] = self.data[largest], self.data[index]
+                index = largest
+            else:
+                break
 
     def insert(self, value):
-
-        # TODO: implement core, value, up
-        pass
+        self.data.append(value)
+        self._heapify_up(len(self.data) - 1)
 
     def extract_max(self):
-
-        # TODO: implement, swap root, down
-        pass
-
-    def peek_max(self):
-
         if not self.data:
             return None
-        return self.data[0]
+
+        if len(self.data) == 1:
+            return self.data.pop()
+
+        root = self.data[0]
+        self.data[0] = self.data.pop()
+        self._heapify_down(0)
+
+        return root
+
+    def peek_max(self):
+        return self.data[0] if self.data else None
 
     def __len__(self):
         return len(self.data)
 
     def is_empty(self):
         return len(self.data) == 0
-
-
-if __name__ == "__main__":
-    heap = MaxHeap()
-    heap.insert(10)
-    heap.insert(5)
-    heap.insert(20)
-    print("Current raw heap array:", heap.data)
-    print("Current max:", heap.peek_max())
