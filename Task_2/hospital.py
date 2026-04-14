c9decbbe074cafac3338c206d23f6c32cac83ffc
Kerwin's Cat <hankongdongxu@gmail.com>
2026-04-14 22:20:16 +0800

    Update hospital.py

--- Task_2/hospital.py
+++ Task_2/hospital.py
@@ -9,7 +9,7 @@ class HospitalSystem:
 
     def add_doctor(self, name: str, specialist: str):
         doctor_id = f"D{Doctor.NEXT_ID:03d}"
-        doctor = Doctor(name, specialist)
+        doctor = Doctor(name, specialist, doctor_id)
         self.doctors[doctor.doctor_id] = doctor
         return doctor
 
