1678355fd82aa31c5c92364a43dac10a460f365c
Kerwin's Cat <hankongdongxu@gmail.com>
2026-04-16 19:52:30 +0800

    Comment update

--- Task_2/update_main_program.py
+++ Task_2/update_main_program.py
@@ -4,21 +4,21 @@ from hospital import HospitalSystem
 if __name__ == "__main__":
     hospital = HospitalSystem()
 
-    # 加醫生
+    # Add doctors
     doctor1 = hospital.add_doctor("Doctor Li", "Cardiology")
     doctor2 = hospital.add_doctor("Doctor Chan", "Emergency")
 
-    # 加病人
+    # Add patient
     p1 = hospital.add_patient("ZhangWei", "A123456(0)", 25, Gender.MALE, TriageLevel.EMERGENCY)
     p2 = hospital.add_patient("Wong Mei", "B234567(1)", 60, Gender.FEMALE, TriageLevel.CRITICAL)
     p3 = hospital.add_patient("Chan Tai", "C345678(2)", 40, Gender.MALE, TriageLevel.URGENT)
 
-    # 顯示目前等待順序
+    # Display Current Waiting Order
     print("Waiting list:")
     for pid, level in hospital.get_waiting_list_sorted():
         print(pid, level.name)
 
-    # 逐個交給醫生
+    # Hand them over to the doctor one by one
     print("\nAssign patients:")
     result1 = hospital.assign_next_patient_to_doctor(doctor1.doctor_id)
     print(result1)
